817497ac716efeb541f03c91b3bf659c5eb82887
XiangYu <49023462+xiangyu5632@users.noreply.github.com>
2024-12-01 20:26:49 +0800

    chore: add open-source star (#40)
    add open-source star
    
    Signed-off-by: xiangyu5632 <xiangyu5632@126.com>

--- docs/.vuepress/theme/pages/opensourcestar/starInfo.ts
+++ docs/.vuepress/theme/pages/opensourcestar/starInfo.ts
@@ -1,5 +1,50 @@
 export const starInfo = [
     {
+        year: 2024,
+        members: [
+            {
+                name: '莫晓颖@CennyMo',
+                name_en: 'Xiaoying Mo @CennyMo',
+                img: '/images/stars/moxiaoying.jpeg',
+                intro: '作为一名女性开发者，我对开源世界充满热情，专注于提高openGemini代码的质量和优化用户的体验，通过不断地学习新知和实践应用，期望能为openGemini社区注入更多创新思维和有效的解决方案。',
+                intro_en:
+                    'As a female developer, I am passionate about the open-source world and focus on improving the quality of openGemini code and optimizing user experience. I hope to inject more innovative thinking and effective solutions into the openGemini community by constantly learning new knowledge and applying it in practice.',
+            },
+            {
+                name: '泽瑞坤@hiiiik',
+                name_en: 'Ruikun Ze @hiiiik',
+                img: '/images/stars/zeruikun.jpg',
+                intro: '我在社区主要承担openGemini C++ Client（SDK）的开发工作，并根据用户的反馈进行优化，以提升性能和稳定性，同时积极参与社区讨论，分享我的专业知识，帮助解决其他开发者遇到的问题。',
+                intro_en:
+                    'In the community, I am mainly responsible for developing the openGemini C++ Client (SDK) and optimizing it based on user feedback to improve performance and stability. At the same time, I actively participate in community discussions, share my expertise, and help solve problems encountered by other developers.',
+            },
+            {
+                name: '高继垚@StepY1aoZz',
+                name_en: 'Jiyao Gao @StepY1aoZz',
+                img: '/images/stars/gaojiyao.jpeg',
+                intro: '我正在基于openGemini研究乱序数据的高效处理方法，同时也参与了部分特性的开发工作。openGemini一个充满活力和友好氛围的社区，这里的每个人都对开源技术充满热情，我期待在未来的日子里，与社区一同成长。',
+                intro_en:
+                    'I am currently researching efficient methods for processing out-of-order data based on openGemini and am also involved in developing some features. openGemini is a vibrant and friendly community where everyone is passionate about open-source technology, and I look forward to growing with the community in the future.',
+            },
+            {
+                name: '王春凯@rumor-sourse',
+                name_en: 'Chunkai Wang @rumor-sourse',
+                img: '/images/stars/wangchunkai.jpeg',
+                intro: '我是从开源之夏活动中了解openGemini，第一次接触数据库开发，在导师的指导下完成了数据导出特性的开发工作。通过这个过程，我不仅提升了自己的编程技能，也对数据库的内部工作原理有了更深入的理解。',
+                intro_en:
+                    'I learned about openGemini from the Open Source Summer event, which was my first time engaging in database development. Under the guidance of my mentor, I completed the development of the data export feature. Through this process, I not only improved my programming skills but also gained a deeper understanding of the database\'s internal working principles.',
+            },
+            {
+                name: '陈果滔@cpcchengt',
+                name_en: 'Guotao Chen @cpcchengt',
+                img: '/images/stars/chenguotao.jpeg',
+                intro: '作为开源爱好者，我在openGemini Rust/Python/Java等多个Client（SDK）项目中参与Issue修复和文档维护，重点参与openGemini Rust Client（SDK）。在这个过程中，我不仅深入理解了openGemini的架构和特性，通过这些实践，我对分布式时序数据库有了更深刻的认识，并在实际开发中积累了宝贵的经验',
+                intro_en:
+                    'As an open source enthusiast, I participated in issue repair and document maintenance in multiple Client (SDK) projects such as openGemini Rust/Python/Java, focusing on openGemini Rust Client (SDK). Through these practices, I not only gained a deep understanding of the architecture and features of openGemini but also of distributed time series databases and accumulated valuable experience in actual development.',
+            },
+        ],
+    },
+    {
         year: 2023,
         members: [
             {

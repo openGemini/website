8b77f70f915f9cc4c1247267b69c50ba810df381
Ihor Sychevskyi <arhell333@gmail.com>
2025-01-15 03:39:59 +0200

    feat: update logos for dark theme (#49)
    resolve https://github.com/openGemini/website/issues/48
    
    Signed-off-by: Arhell <arhell333@gmail.com>

--- docs/.vuepress/theme/components/composables/dark.ts
+++ docs/.vuepress/theme/components/composables/dark.ts
@@ -1,4 +1,6 @@
-import { useDark, useToggle } from '@vueuse/core'
+import { useDark, useToggle } from '@vueuse/core';
+import { ref } from 'vue';
 
 export const isDark = useDark()
 export const toggleDark = useToggle(isDark)
+export const isDarkTheme = ref(true)
